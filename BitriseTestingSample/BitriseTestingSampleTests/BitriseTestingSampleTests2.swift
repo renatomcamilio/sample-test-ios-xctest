ac3540051c0143f8aeaa9c4e02dcd044bf7fd837
Rashin Arab <rashin@buddybuild.com>
2015-07-29 19:10:44 -0700

    Adding another test failure

---
+++ BitriseTestingSample/BitriseTestingSampleTests/BitriseTestingSampleTests2.swift
@@ -0,0 +1,9 @@
+//
+//  BitriseTestingSampleTests2.swift
+//  BitriseTestingSample
+//
+//  Created by Rashin Arab on 2015-07-29.
+//  Copyright (c) 2015 Bitrise. All rights reserved.
+//
+
+import Foundation
